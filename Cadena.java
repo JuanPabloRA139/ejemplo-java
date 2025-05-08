9aa57370ae6bd8f0a13158a4957d2e296a0dc3b7
JuanPabloRA139 <20243ds139@utez.edu.mx>
2025-05-08 09:43:10 -0600

    Se agrego un scanner y un print

--- Cadena.java
+++ Cadena.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
-public classs Cadena{
-    public static void main(String[] args){
-        String nombre;
+public class Cadena {
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        System.out.println("Escribe tu nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.println("Escribe tu apellido: ");
+        String apellido = entrada.nextLine();
+        System.out.println("Tu nombre completo es: "+ nombre + apellido);
+        
+
 
     }
 }
