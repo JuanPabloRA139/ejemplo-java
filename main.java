e36c9f1ba3eb22614030a1d92ba05d4adfc82bd2
JuanPabloRA139 <20243ds139@utez.edu.mx>
2025-05-08 08:43:36 -0600

    Primer commit

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Caracteres { 
+    public static void main(String[] args){
+        char letra = 'a';
+        char simbolo = '$';
+        char numeroChar = 'S';
+        System.out.printIn("letra = " + letra);
+         System.out.printIn("simbolo = " + simbolo);
+         System.out.printIn("numeroChar = " + numeroChar);
+    }
+}
