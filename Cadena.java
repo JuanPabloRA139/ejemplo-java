ddbdaa187cb5a14f6a8d400675c67ed98d45fdcf
JuanPabloRA139 <20243ds139@utez.edu.mx>
2025-05-08 09:40:50 -0600

    Se agrego una entrada y un print

---
+++ Cadena.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public classs Cadena{
+    public static void main(String[] args){
+        String nombre;
+        Scanner entrada = new Scanner(System.in);
+
+    }
+}
